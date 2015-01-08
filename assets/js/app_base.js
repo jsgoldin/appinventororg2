a064f5446bf184160d00a8fba2a0d5f2da198ef3
jsgoldin <jorgoldin@gmail.com>
2015-01-08 15:07:30 -0800

    Implemented only one video play at a time on app video tutorial pages

--- assets/js/app_base.js
+++ assets/js/app_base.js
@@ -1,12 +1,12 @@
 // load the IFrame Player API code asynchronously.
-
 var tag = document.createElement('script');
-
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-
+/**
+ * Parses and returns the video id from a youtube video url.
+ */
 function getUTUBEID(url) {
 	id_start = url.indexOf('/v/') + 3;
 	url = url.slice(id_start);
@@ -20,77 +20,94 @@ function getUTUBEID(url) {
 }
 
 
+var videoArray = [];	/* maintains a reference to all videos on the page. */
 
-// load the videos
+/**
+ * Load the videos
+ */
 function onYouTubeIframeAPIReady() {
 	// iterate through all the things django put in
 	$('#carousel-wrapper').children().each(function() {
-	
-	
-	
 		videotarget = $(this).find('.carousel-step-video');
-
 		desiredHeight = videotarget.width() * 0.56;
-
 		videotarget.height(desiredHeight);
-
 		video_id = getUTUBEID(videotarget.attr('data-utubeurl'));
-
 		target_div = videotarget[0];
 		player = new YT.Player(target_div, {
 			height : '400px',
 			width : 'auto',
 			videoId : video_id,
-			playerVars: {
-            	'showinfo': 0
-        	}
+			playerVars : {
+				'showinfo' : 0
+			}
 		});
+		
+		videoArray.push(player);
+		
 	});
 
 	$("#carousel-wrapper").owlCarousel({
-	      slideSpeed : 300,
-	      paginationSpeed : 400,
-	      singleItem:true,
-	      paginationNumbers: true,
-	      lazyLoad : true
-	  });
+		slideSpeed : 300,
+		paginationSpeed : 400,
+		singleItem : true,
+		paginationNumbers : true,
+		lazyLoad : true,
+		afterAction : stopAllVideos,
+	});
+	
+	
+	function stopAllVideos() {
+		for (var i = 0; i < videoArray.length; i++) {
+		    videoArray[i].pauseVideo();
+		}
+	}
 	
 	// add custom navigation buttons
-	
-	$('.owl-pagination').prepend("<div id='carousel-nav-btn-left' class='carousel-nav-btn'><span class='glyphicon glyphicon-chevron-left'></span></div>");
-	$('.owl-pagination').append("<div id='carousel-nav-btn-right' class='carousel-nav-btn'><span class='glyphicon glyphicon-chevron-right'></span></div>");
+	$('.owl-pagination').prepend(
+		"<div id='carousel-nav-btn-left' class='carousel-nav-btn'>" +
+		"	<span class='glyphicon glyphicon-chevron-left'>" +
+		"	</span>" +
+		"</div>");
+	$('.owl-pagination').append(
+		"<div id='carousel-nav-btn-right' class='carousel-nav-btn'>" +
+		"	<span class='glyphicon glyphicon-chevron-right'>" +
+		"	</span>" +
+		"</div>");
 	return player;
 }
 
 
 
 
-
-
 var i = 1;
 $(window).resize(function() {
-	if(i % 2==0) {
+	if (i % 2 == 0) {
 		setTimeout(function() {
-		
-			if($('#carousel-nav-btn-left').length == 0) {
-				$('.owl-pagination').prepend("<div id='carousel-nav-btn-left' class='carousel-nav-btn'><span class='glyphicon glyphicon-chevron-left'></span></div>");
-			}
-
-			if($('#carousel-nav-btn-right').length == 0) {
-				$('.owl-pagination').append("<div id='carousel-nav-btn-right' class='carousel-nav-btn'><span class='glyphicon glyphicon-chevron-right'></span></div>");
-     		}
-     }, 700);
-	}
-	i++;
+		if ($('#carousel-nav-btn-left').length == 0) {
+			$('.owl-pagination').prepend(
+			"<div id='carousel-nav-btn-left' class='carousel-nav-btn'>" +
+				"<span class='glyphicon glyphicon-chevron-left'>" +
+				"</span>" +
+			"</div>");
+		}
+		if ($('#carousel-nav-btn-right').length == 0) {
+			$('.owl-pagination').append(
+			"<div id='carousel-nav-btn-right' class='carousel-nav-btn'>" +
+			"	<span class='glyphicon glyphicon-chevron-right'>" +
+			"	</span>" +
+			"</div>");
+		}
+	}, 700);
+}
+i++;
 });
 
 
 
-$(document).on("click","#carousel-nav-btn-left",function() {
-	$(".owl-carousel").data('owlCarousel').prev();
+$(document).on("click", "#carousel-nav-btn-left", function() {
+	$(".owl-carousel").data('owlCarousel').prev();	
 });
 
-
-$(document).on("click","#carousel-nav-btn-right",function() {
+$(document).on("click", "#carousel-nav-btn-right", function() {
 	$(".owl-carousel").data('owlCarousel').next();
 });
